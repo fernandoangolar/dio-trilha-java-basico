2373af21485a72e797369b097739a224ec3f2f74
Fernando Angolar <fernandoangolar581@gmail.com>
2024-08-14 18:56:58 +0100

    conta finalizad

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * ContaTerminal
@@ -5,7 +6,26 @@
 public class ContaTerminal {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Por favor, ");
+
+        System.out.print("Por favor, digite o numero: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o seu Saldo : ");
+        double saldo = scanner.nextDouble();
+
+        System.out.print("Por favor, digite o número da Agênda: ");
+        String agenda = scanner.next();
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.next();
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenda + ", conta " + numero + "  e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
